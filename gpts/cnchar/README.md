b7a1027c492ce6a6e6fc60a8792e17e0401d1784
Yvo <akira.cn@gmail.com>
2024-01-15 18:37:46 +0800

    Update via AirCode on Jan 15, 2024

---
+++ gpts/cnchar/README.md
@@ -0,0 +1,15 @@
+## Example
+
+https://chat.openai.com/g/g-HhhuZ8uIY-learn-chinese-character
+
+## Configuration
+
+1. Create action.
+2. Import JSON Sechma: `https://znnm5f51mc.us.aircode.run/schema`.
+3. Set privacy-policy: `https://znnm5f51mc.us.aircode.run/policy`.
+
+## One-Click Deployment
+
+Easily get a copy and launch your app on [AirCode](https://aircode.io/) by clicking the button below.
+
+[![Deploy with AirCode](https://aircode.io/aircode-deploy-button.svg)](https://aircode.io/dashboard?owner=AirCodeLabs&repo=Awesome-actions-for-GPTs&path=gpts%2Fcnchar&appname=cnchar%20for%20gpts)
---
+++ gpts/cnchar/draw.js
@@ -0,0 +1,35 @@
+const aircode = require('aircode');
+
+require('jsdom-global')()
+const cnchar = require('cnchar');
+
+module.exports = async function (text) {
+  document.body.innerHTML = '<div id="drawStroke"></div>';
+
+  const draw = require('cnchar-draw');
+  cnchar.use(draw);
+  
+  await new Promise((resolve) => {
+    cnchar.draw(text,{
+      el: '#drawStroke',
+      type: cnchar.draw.TYPE.STROKE,
+      // type: cnchar.draw.TYPE.ANIMATION,
+      // animation:{
+      //     loopAnimate: true
+      // },
+      onComplete: () => {
+        // console.log('complete');
+        resolve();
+      },
+    });
+  });
+
+  const svgs = [...document.querySelectorAll('svg')];
+  const res = svgs.map(async (svg) => {
+    svg.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
+    return await aircode.files.upload(svg.outerHTML, `${Math.random().toString(36).slice(2, 8)}.svg`, {
+    type: 'image/svg+xml',
+  })});
+
+  return await Promise.all(res);
+};
