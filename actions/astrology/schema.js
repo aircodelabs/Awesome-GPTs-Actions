52bfb9ec1c61e79805d9624d1ff6894593545845
Yvo <akira.cn@gmail.com>
2024-01-15 18:33:12 +0800

    Update via AirCode on Jan 15, 2024

--- actions/astrology/schema.js
+++ actions/astrology/schema.js
@@ -3,6 +3,8 @@ const aircode = require('aircode');
 const fs = require('node:fs');
 
 module.exports = async function (params, context) {
-  const data = fs.readFileSync('./meta.json', 'utf8');
-  return JSON.parse(data);
+  const data = fs.readFileSync('./schema.json', 'utf8');
+  const ret = JSON.parse(data);
+  ret.servers[0].url = `https://${context.request.host}`;
+  return ret;
 };
